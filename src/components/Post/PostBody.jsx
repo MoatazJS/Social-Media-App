c96edb38eaef479224488812ad371f44a653a1c0
Moataz <moataz_abdullah@outlook.com>
2025-08-22 18:56:12 +0300

    finished structuring Post component

---
+++ src/components/Post/PostBody.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function PostBody({ caption, image }) {
+  return (
+    <>
+      {caption && <p>{caption}</p>}
+      {image && (
+        <img src={image} className="w-full h-100 object-cover mt-2" alt="" />
+      )}
+    </>
+  );
+}
