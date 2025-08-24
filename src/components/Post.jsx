35fbe25782669821796bac99e793bb5a99730a3d
Moataz <moataz_abdullah@outlook.com>
2025-08-25 01:11:53 +0300

    added show more button to show comments in post details

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,19 @@ import PostBody from "./Post/PostBody";
 import PostFooter from "./Post/PostFooter";
 import PostActions from "./Post/PostActions";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Button, Input } from "@heroui/react";
 export default function Post({ post, commentsLimit }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [visibleComments, setVisibleComments] = useState(5);
+  const showMore = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setVisibleComments(visibleComments + 10);
+      setIsLoading(false);
+    }, 500);
+  };
+
   const navigate = useNavigate();
   const openPostDetails = (e) => {
     if (e.currentTarget.id === "post-card") {
@@ -43,9 +55,24 @@ export default function Post({ post, commentsLimit }) {
       <PostBody caption={post.body} image={post.image} />
       <PostFooter numOfComments={post.comments.length} />
       <PostActions />
-      {post.comments.slice(0, commentsLimit).map((comment) => (
-        <Comment key={comment.id} comment={comment} />
-      ))}
+      <div className="flex">
+        <Input />
+      </div>
+      {post.comments
+        .slice(0, commentsLimit ?? visibleComments)
+        .map((comment) => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
+      {visibleComments < post.comments.length && !commentsLimit && (
+        <Button
+          isLoading={isLoading}
+          onPress={showMore}
+          className="block mx-auto"
+          variant="faded"
+        >
+          Show More
+        </Button>
+      )}
     </div>
   );
 }
